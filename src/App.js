1cf6c3b0d0b518f3bcd5199f4979a3246928f15b
eprikhodko <eprikhodkodev@gmail.com>
2021-04-24 14:38:12 +0300

    Add useContext() hook

--- src/App.js
+++ src/App.js
@@ -2,20 +2,12 @@ import React from "react"
 
 import Header from "./Header"
 import Button from "./Button"
-import {ThemeContextConsumer} from "./themeContext"
 
 function App() {
     return (
         <div>
             <Header />
-
-            <ThemeContextConsumer>
-                {theme => (
-                  <Button theme={theme}/>
-                )}
-            </ThemeContextConsumer>
-
-            <Button theme="dark"/>
+            <Button />
         </div>
     )
 }
